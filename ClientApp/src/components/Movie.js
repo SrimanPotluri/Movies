a70c84507a4bc8c0ab6870666fc7730e91f6c805
Administrator <Administrator@EC2AMAZ-JIJ2EN0>
2021-10-31 23:20:46 +0000

    Pushed search bar to the right and changed text color of move-over tag

--- ClientApp/src/components/Movie.js
+++ ClientApp/src/components/Movie.js
@@ -8,7 +8,7 @@ import axios from 'axios';
 const Movie = ({ titleName, releaseYear, overview }) =>
 
     <div className="movie">
-        <img src="https://images.unsplash.com/photo-1440404653325-ab127d49abc1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2370&q=80"/>
+        <img src="https://images.unsplash.com/photo-1598899134739-24c46f58b8c0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1156&q=80"/>
         <div className="movie-info">
         <h3>
            {titleName}
@@ -16,8 +16,8 @@ const Movie = ({ titleName, releaseYear, overview }) =>
             <span>{releaseYear}</span>
         </div>
         <div className="movie-over">
-            <h3>Overview:</h3>
-            <p>Contemporary climate change involves rising global temperatures and significant shifts in Earth's weather patterns. Climate change is driven by emissions of heat-trapping greenhouse gases such as carbon dioxide and methane. Emissions come mostly from burning fossil fuels (coal, oil and natural gas), and also from agriculture, forest loss, cement production and steel making. Climate change causes sea level rise, glacial retreat and desertification, and intensifies heat waves, wildfires and tropical storms. These effects of climate change endanger food security, freshwater access and global health. Climate change can be limited by using low-carbon energy sources such as wind and solar energy, forestation, and shifts in agriculture. Adaptations such as coastline protection cannot by themselves avert the risk of severe, pervasive and irreversible impacts</p>
+            <h4>Overview:</h4>
+            <p>dsgbffsd;uiohdsu;h;uoifashg;o</p>
         </div>
     </div>;
 
