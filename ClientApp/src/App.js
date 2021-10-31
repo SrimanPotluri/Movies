e029ccf1efd24f67f70f54f47d63760d8034f406
Administrator <Administrator@EC2AMAZ-JIJ2EN0>
2021-10-31 21:50:09 +0000

    Modified UI - Part1

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,22 +1,12 @@
 import React, { Component } from 'react';
+import { Container } from 'reactstrap';
 import { Route } from 'react-router';
-import { Layout } from './components/Layout';
-import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
+import Movies from './components/Movies';
 
 import './custom.css'
 
-export default class App extends Component {
-  static displayName = App.name;
-
-  render () {
-    return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
-      </Layout>
-    );
-  }
+const App = () => {
+    return <Container><Movies/></Container>
 }
+
+export default App;
